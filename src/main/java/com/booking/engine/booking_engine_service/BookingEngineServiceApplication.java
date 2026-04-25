2c8f9a94e53f8f8210edab4b60b77bdd36d57830
Raushan1156 <iamraushankumar56@gmail.com>
2026-04-25 20:07:26 +0530

    first commit

---
+++ src/main/java/com/booking/engine/booking_engine_service/BookingEngineServiceApplication.java
@@ -0,0 +1,13 @@
+package com.booking.engine.booking_engine_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingEngineServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingEngineServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/booking/engine/booking_engine_service/BookingEngineServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.booking.engine.booking_engine_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingEngineServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
