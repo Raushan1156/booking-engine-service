4fc16e1c33c9346eacbde1d5547e0b43ccbc25e1
Raushan1156 <iamraushankumar56@gmail.com>
2026-06-07 14:04:59 +0530

    added logger, handler and formatter

--- src/test/java/com/booking/engine/booking_engine_service/LoggingTest.java
+++ src/test/java/com/booking/engine/booking_engine_service/LoggingTest.java
@@ -7,6 +7,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.UUID;
+
 @SpringBootTest
 //@Slf4j
 public class LoggingTest {
@@ -30,5 +32,37 @@ public class LoggingTest {
         log.error("This is ERROR level.");
         log.debug("This is DEBUG level.");
         log.trace("This is TRACE level.");
+    }
+
+    @Test
+    @DisplayName("Mock Transaction Flow")
+    void loggerMethods(){
+        String token = UUID.randomUUID().toString().substring(0,7);
+        log.trace("[Token:{}] - Entering into a Mock Transaction Flow.", token);
+
+        try {
+            String accountType = "Premium";
+            int activeAccount = 10;
+            double transactionLimitEachDay = 100000.00;
+            double currentTransaction = 90000.00;
+            log.debug("Current data: {}, {}", currentTransaction, transactionLimitEachDay);
+            double leftVolume = transactionLimitEachDay - currentTransaction;
+            if (leftVolume <= 11000.50) {
+                log.warn("Transaction limit is about to exhaust. Left volume is:{}", leftVolume);
+            } else if (leftVolume<=0) {
+                throw new RuntimeException("check the left volumn.");
+            } else {
+                log.info("{} has been deducted.", currentTransaction);
+            }
+            log.debug("Initial volume was: {},Left volume is: {}", transactionLimitEachDay, leftVolume);
+            log.info("Limit / volume has been updated.");
+        }
+        catch(Exception ex){
+            log.error("Limit has been exceeded...");
+            }
+        finally {
+            log.trace("Exiting the volume tracking scope...");
+        }
+
     }
 }
